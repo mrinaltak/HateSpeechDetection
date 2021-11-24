80a869e6274c267d450f68fe80ee9d40f16d2131
Mrinal Tak <mrinaltak@users.noreply.github.com>
2021-11-24 17:48:18 -0500

    cleaned train dataset

--- generate_datasets.py
+++ generate_datasets.py
@@ -1,6 +1,6 @@
 from datasets import load_dataset
 
-train_dataset = load_dataset('csv', data_files={'train': './OLID_dataset/olid-training-v1.0.tsv'}, delimiter="\t")
+train_dataset = load_dataset('csv', data_files={'train': './OLID_dataset/olid_train_v2.csv'}, delimiter="\t")
 test_a_dataset = load_dataset('csv', data_files={'test': './OLID_dataset/olid_test_subtask_a.csv'}, delimiter="\t")
 test_b_dataset = load_dataset('csv', data_files={'test': './OLID_dataset/olid_test_subtask_b.csv'}, delimiter="\t")
 test_c_dataset = load_dataset('csv', data_files={'test': './OLID_dataset/olid_test_subtask_c.csv'}, delimiter="\t")
