1736d16084510714489d1073644b08c18e52ff28
Mrinal Tak <mrinaltak@users.noreply.github.com>
2021-11-23 23:19:41 -0500

    fixed the paths

--- generate_datasets.py
+++ generate_datasets.py
@@ -1,6 +1,6 @@
 from datasets import load_dataset
 
-train_dataset = load_dataset('csv', data_files={'train': './OLIDv1/olid-training-v1.0.tsv'}, delimiter="\t")
-test_a_dataset = load_dataset('csv', data_files={'test': 'olid_test_subtask_a.csv'}, delimiter="\t")
-test_b_dataset = load_dataset('csv', data_files={'test': 'olid_test_subtask_b.csv'}, delimiter="\t")
-test_c_dataset = load_dataset('csv', data_files={'test': 'olid_test_subtask_c.csv'}, delimiter="\t")
+train_dataset = load_dataset('csv', data_files={'train': './OLID_dataset/olid-training-v1.0.tsv'}, delimiter="\t")
+test_a_dataset = load_dataset('csv', data_files={'test': './OLID_dataset/olid_test_subtask_a.csv'}, delimiter="\t")
+test_b_dataset = load_dataset('csv', data_files={'test': './OLID_dataset/olid_test_subtask_b.csv'}, delimiter="\t")
+test_c_dataset = load_dataset('csv', data_files={'test': './OLID_dataset/olid_test_subtask_c.csv'}, delimiter="\t")
