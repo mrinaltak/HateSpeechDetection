a59457c2213aafe1f2bb8891f8b4b4687dd92811
ashantanu <a.shantanu08@gmail.com>
2021-12-05 00:21:39 +0000

    create val dataset

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ All possible label combinations across all the sub-tasks are as below:
   Not offensive
   Offensive, Untargeted
   Offensive, Targeted Insult, (Individual | Group | Other)
+
+
+## Setup
+
+- install required packages `pip install -r requirements.txt`
---
+++ split_dataset.py
@@ -0,0 +1,19 @@
+import pandas as pd
+train = pd.read_csv('./OLID_dataset/olid_train_v2.csv',sep='\t')
+val = train.sample(frac=0.065,replace=False)
+final = pd.concat([train, val, val]).drop_duplicates(keep=False)
+
+#sanity checks
+assert len(final) == 13240 - len(val)
+s1 = pd.merge(val, final, how='inner', on=['id'])
+assert len(s1)==0
+
+s1 = pd.merge(final, train, how='inner', on=['id'])
+assert len(s1) == len(final)
+
+s1 = pd.merge(val, train, how='inner', on=['id'])
+assert len(s1) == len(val)
+
+val.to_csv('./OLID_dataset/olid_val_v2.csv',sep='\t')
+final.to_csv('./OLID_dataset/olid_train_v3.csv',sep='\t')
+
